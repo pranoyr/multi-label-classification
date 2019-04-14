a72693419d24be0986ab1b0fc381d68668336b72
pranoy <pranoyalkr@gmail.com>
2019-04-14 13:07:54 +0000

    updated

--- predict.py
+++ predict.py
@@ -3,10 +3,10 @@ from keras.models import load_model
 from PIL import Image
 import numpy as np
 from utils import pre_process
-
+import scipy.io
 
 # creating names to labels and inverse mapping.
-labels_data = scipy.io.loadmat('/Users/pranoyr/PycharmProjects/multi-label-classification/dataset/label_list.mat')
+labels_data = scipy.io.loadmat('./dataset/label_list.mat')
 names_to_labels = {}
 for k,i in enumerate(labels_data['label_list'][0]):
     names_to_labels.update({i[0]:k})
@@ -15,7 +15,7 @@ labels_to_names = {v: k for k, v in names_to_labels.items()}
 
 
 model = load_model('./snapshots/human_attribute_model_20.h5')
-img_rgb  = Image.open('./dataset/photos/1005.jpg')
+img_rgb  = Image.open('./sample.jpg')
 img_rgb = img_rgb.resize((100, 200))
 
 img_batch = np.expand_dims(img_rgb, axis=0)
@@ -23,5 +23,5 @@ img_batch = pre_process(img_batch)
 
 results = model.predict(img_batch)
 for i,score in enumerate(results[0]):
- if (score > 0.8):
+ if (score > 0.7):
     print(labels_to_names[i])
